62a0cba577f16361847c99b08b99e3df716e0870
Kristoffer Bjärkefur <kbjarkefur@users.noreply.github.com>
2023-09-20 06:22:04 -0400

    pkg meta info - make fit adodown pkg template

--- R/metadata.R
+++ R/metadata.R
@@ -16,32 +16,32 @@ find_file_in_pkg <- function(
 
 get_pkg_metadata <- function(pkg_file) {
 
-    pkg_file <- readLines(
+    pkg_lines <- readLines(
         pkg_file,
         warn = FALSE # suppress warning to avoid confusion
     )
 
-    version <- stringr::str_subset(pkg_file, "^v")[1] |>
-        stringr::str_replace("v (.+)", "\\1")
-    name    <- stringr::str_subset(pkg_file, "^d")[1] |>
-        stringr::str_replace("d (.+)", "\\1")
-    title   <- stringr::str_subset(pkg_file, "^d")[2] |>
-        stringr::str_replace("d (.+)", "\\1")
-    desc    <- stringr::str_subset(pkg_file, "^d")[3] |>
-        stringr::str_replace("d (.+)", "\\1")
-    authors <- stringr::str_subset(pkg_file, "^d")[4] |>
-        stringr::str_replace("d (.+)", "\\1")
-    url     <- stringr::str_subset(pkg_file, "^d")[5] |>
-        stringr::str_replace("d (.+)", "\\1")
+    for (i in 1:length(pkg_lines)) {
+        if ('*** version' %in% pkg_lines[i])     pkg_v   <- stringr::str_remove(pkg_lines[i+1],"v ")
+        if ('*** stata' %in% pkg_lines[i])       stata_v <- stringr::str_remove(pkg_lines[i+1],"d Version: ")
+        if ('*** name' %in% pkg_lines[i])        name    <- stringr::str_remove(pkg_lines[i+1],"d ")
+        if ('*** date' %in% pkg_lines[i])        date    <- stringr::str_remove(pkg_lines[i+1],"d Distribution-Date: ")
+        if ('*** description' %in% pkg_lines[i]) desc    <- stringr::str_remove(pkg_lines[i+1],"d ")
+        if ('*** author' %in% pkg_lines[i])      author  <- stringr::str_remove(pkg_lines[i+1],"d Author:")
+        if ('*** contact' %in% pkg_lines[i])     contact <- stringr::str_remove(pkg_lines[i+1],"d Contact: ")
+        if ('*** url' %in% pkg_lines[i])         url     <- stringr::str_remove(pkg_lines[i+1],"d URL: ")
+    }
 
     pkg <- list(
-        version = version,
+        pkg_v = pkg_v,
+        stata_v = stata_v,
         name = name,
+        date = date,
         desc = desc,
-        authors = authors,
+        author = author,
+        contact = contact,
         url = url
     )
 
     return(pkg)
-
 }
